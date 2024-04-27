ead90fdc0c3d2258e1a2a4502d1d502eafa11e13
04990672070 <04990672070@senacrs.edu.br>
2024-04-26 21:45:46 -0300

    Commmit

--- index.js
+++ index.js
@@ -1,81 +1,74 @@
-const dados = require('./dados.json')
-const express = require('express')
-const fs = require('fs')
-const cors = require('cors')
-const path = require('path');
-const swaggerUi = require('swagger-ui-express');    
-const swaggerDocument = require('./swagger.json');
+const dados = require('./dados.json');
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
 
-const server = express()
-server.use(cors())
-server.use(express.json())
+const server = express();
+server.use(cors());
+server.use(express.json());
 
-server.listen(3000, () => {
+server.listen(3001, () => {
     console.log("O servidor está funcional");
-})
+});
 
 server.get('/', (req, res) => {
-    return res.json({ mensagem: "Estou funcionando!" })
-})
-
-// CRUD DA API
+    return res.json({ mensagem: "Estou funcionando!" });
+});
 
 // Create da API
 server.post('/usuarios', (req, res) => {
-    const novoUsuario = req.body
+    const novoUsuario = req.body;
 
     if (!novoUsuario.nome || !novoUsuario.idade || !novoUsuario.curso) {
-        return res.status(400).json({ mensagem: "Dados incompletos, tente novamente" })
+        return res.status(400).json({ mensagem: "Dados incompletos, tente novamente" });
     } else {
-        dados.Usuarios.push(novoUsuario)
-        salvarDados(dados)
+        novoUsuario.id = dados.Usuarios.length + 1;
+        dados.Usuarios.push(novoUsuario);
+        salvarDados(dados);
 
-        return res.status(201).json({ mensagem: "Dados completos, cadastro feito com sucesso!" })
+        return res.status(201).json({ mensagem: "Cadastro feito com sucesso!", usuario: novoUsuario });
     }
-})
+});
 
 // Read da API
 server.get('/usuarios', (req, res) => {
-    return res.json(dados.Usuarios)
-})
+    return res.json(dados.Usuarios);
+});
 
 // Update da API
 server.put('/usuarios/:id', (req, res) => {
-    const usuarioId = parseInt(req.params.id)
-    const atualizarUser = req.body
+    const usuarioId = parseInt(req.params.id);
+    const atualizarUser = req.body;
 
-    const indiceUsuario = dados.Usuarios.findIndex(u => u.id === usuarioId)
+    const indiceUsuario = dados.Usuarios.findIndex(u => u.id === usuarioId);
 
     if (indiceUsuario === -1) {
-        return res.status(404).json({ mensagem: "Usuário não encontrado" })
+        return res.status(404).json({ mensagem: "Usuário não encontrado" });
     } else {
-        dados.Usuarios[indiceUsuario].nome = atualizarUser.nome || dados.Usuarios[indiceUsuario].nome
-        
-        dados.Usuarios[indiceUsuario].idade = atualizarUser.idade || dados.Usuarios[indiceUsuario].idade
+        dados.Usuarios[indiceUsuario] = { ...dados.Usuarios[indiceUsuario], ...atualizarUser };
+        salvarDados(dados);
 
-        dados.Usuarios[indiceUsuario].curso = atualizarUser.curso || dados.Usuarios[indiceUsuario].curso
-
-        salvarDados(dados)
-
-        return res.status(201).json({ mensagem: "Dados completos, atualização feita com sucesso!" })
+        return res.status(200).json({ mensagem: "Atualização feita com sucesso!", usuario: dados.Usuarios[indiceUsuario] });
     }
-})
+});
 
-
-//Delete da API
+// Delete da API
 server.delete('/usuarios/:id', (req, res) => {
-    const id = parseInt(req.params.id)
+    const usuarioId = parseInt(req.params.id);
 
-    // filtrar os usuarios, removendo pelo id correspondente
+    const indiceUsuario = dados.Usuarios.findIndex(u => u.id === usuarioId);
 
-    dados.Usuarios = dados.Usuarios.filter(u => u.id !== id)
+    if (indiceUsuario === -1) {
+        return res.status(404).json({ mensagem: "Usuário não encontrado" });
+    } else {
+        dados.Usuarios.splice(indiceUsuario, 1);
+        salvarDados(dados);
 
-    salvarDados(dados)
-
-    return res.status(200).json({ mensagem: "Usuario excluido com sucesso!" })
-})
+        return res.status(200).json({ mensagem: "Usuário excluído com sucesso!" });
+    }
+});
 
 // Função que salva os dados
-function salvarDados() {
-    fs.writeFileSync(__dirname + '/dados.json', JSON.stringify(dados, null, 2))
+function salvarDados(dados) {
+    fs.writeFileSync('./dados.json', JSON.stringify(dados, null, 2));
 }
